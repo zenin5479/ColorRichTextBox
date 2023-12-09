f91f032044f91b23b2846d438728fbb55e3ea0a8
zenin5479 <zenin5479@gmail.com>
2023-12-09 18:28:48 +0300

    Update 1.0 Обновлен метод

--- FormOne.cs
+++ FormOne.cs
@@ -17,14 +17,14 @@ namespace ColorRichTextBox
             RichTextBoxColor("Вывод текста через метод RichTextBoxColor.\n", Color.Red);
         }
 
-        // Вывод текста через метод SavelogColor класса RichTextBoxColor
+        // Вывод текста через метод TextColor класса RichTextBoxColor
         private void ButtonTextTwo_Click(object sender, EventArgs e)
         {
             RichTextBoxOne.TextColor("Вывод текста через метод TextColor класса RichTextBoxColor.\n",
                 Color.Fuchsia);
         }
 
-        // Вывод текста через метод SavelogColor2 класса RichTextBoxColor
+        // Вывод текста через метод TextColor2 класса RichTextBoxColor
         private void ButtonThree_Click(object sender, EventArgs e)
         {
             Font font = new Font("Tahoma", 10, FontStyle.Regular);
@@ -32,7 +32,7 @@ namespace ColorRichTextBox
                 Color.Black, font);
         }
 
-        // Вывод текста через метод SavelogColor3 класса RichTextBoxColor
+        // Вывод текста через метод TextColor3 класса RichTextBoxColor
         private void ButtonFour_Click(object sender, EventArgs e)
         {
             RichTextBoxOne.TextColor3("Вывод текста через метод TextColor3 класса RichTextBoxColor.\n",
