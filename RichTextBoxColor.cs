0529cbaeba6fc573380045e29a8a3d7bd94895be
zenin5479 <zenin5479@gmail.com>
2026-02-18 09:58:01 +0300

    Update 2.6 Обновлен метод

--- RichTextBoxColor.cs
+++ RichTextBoxColor.cs
@@ -18,14 +18,22 @@ namespace ColorRichTextBox
 
       public static void TextColorTwo(RichTextBox rtb, string text, Color color, Font font, bool isNewLine = false)
       {
-         rtb.SuspendLayout(); // Временно приостанавливает логику макета для элемента управления
-         rtb.SelectionStart = rtb.TextLength; // Получает или задает начальную позицию текста, выбранного в текстовом поле
-         rtb.SelectionLength = 0; // Получает или задает число символов, выделенных в элементе управления
-         rtb.SelectionColor = color; // Получает или задает цвет текста, который будет применен к текущему выделению или положению курсора
-         rtb.SelectionFont = font; // Получает или задает цвет текущего текстового выделения или места вставки
-         rtb.AppendText(isNewLine ? string.Format("{0}{1}", text, Environment.NewLine) : text); // Добавляет текст в конец текущего текста в текстовом поле и переходит на новую строку
-         rtb.ScrollToCaret(); // Прокручивает содержимое элемента управления до текущей позиции курсора
-         rtb.ResumeLayout(); // Возобновляет обычную логику макета
+         // Временно приостанавливает логику макета для элемента управления
+         rtb.SuspendLayout();
+// Получает или задает начальную позицию текста, выбранного в текстовом поле
+         rtb.SelectionStart = rtb.TextLength; 
+// Получает или задает число символов, выделенных в элементе управления
+         rtb.SelectionLength = 0; 
+ // Получает или задает цвет текста, который будет применен к текущему выделению или положению курсора
+         rtb.SelectionColor = color; 
+        // Получает или задает цвет текущего текстового выделения или места вставки
+         rtb.SelectionFont = font;
+// Добавляет текст в конец текущего текста в текстовом поле и переходит на новую строку
+         rtb.AppendText(isNewLine ? string.Format("{0}{1}", text, Environment.NewLine) : text);
+// Прокручивает содержимое элемента управления до текущей позиции курсора
+         rtb.ScrollToCaret();
+// Возобновляет обычную логику макета
+         rtb.ResumeLayout(); 
       }
 
       public static void TextColorThree(RichTextBox rtb, string text, Color color)
