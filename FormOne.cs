f3b1e4296eab03e842797667724a8970bf054642
zenin5479 <zenin5479@gmail.com>
2026-02-18 09:54:27 +0300

    Update 2.3 Обновлен метод

--- FormOne.cs
+++ FormOne.cs
@@ -17,20 +17,20 @@ namespace ColorRichTextBox
          RichTextBoxColor("Вывод текста через метод RichTextBoxColor.\n", Color.Red);
       }
 
-      // Вывод текста разных цветов через метод TextColor класса RichTextBoxColor
+      // Вывод текста разных цветов через метод TextColorOne класса RichTextBoxColor
       private void ButtonTextTwo_Click(object sender, EventArgs e)
       {
          ColorRichTextBox.RichTextBoxColor.TextColorOne(RichTextBoxOne, "Вывод текста через метод TextColor класса RichTextBoxColor.\n", Color.Fuchsia);
       }
 
-      // Вывод текста разных цветов через метод TextColor2 класса RichTextBoxColor
+      // Вывод текста разных цветов через метод TextColorTwo класса RichTextBoxColor
       private void ButtonTextThree_Click(object sender, EventArgs e)
       {
          Font font = new Font("Tahoma", 10, FontStyle.Regular);
          ColorRichTextBox.RichTextBoxColor.TextColorTwo(RichTextBoxOne, "Вывод текста через метод TextColor2 класса RichTextBoxColor.\n", Color.Black, font);
       }
 
-      // Вывод текста разных цветов через метод TextColor3 класса RichTextBoxColor
+      // Вывод текста разных цветов через метод TextColorThree класса RichTextBoxColor
       private void ButtonTextFour_Click(object sender, EventArgs e)
       {
          ColorRichTextBox.RichTextBoxColor.TextColorThree(RichTextBoxOne, "Вывод текста через метод TextColor3 класса RichTextBoxColor.\n", Color.Chocolate);
