73d58a4eb4e00e1df4e0e058b841da6002b82965
zenin5479 <zenin5479@gmail.com>
2026-02-23 12:12:58 +0300

    Update 8.2 Обновлен метод

--- RichTextBoxColorConstructor.cs
+++ RichTextBoxColorConstructor.cs
@@ -1,7 +1,56 @@
-﻿namespace ColorRichTextBox
+﻿using System.Windows.Forms;
+using System;
+
+namespace ColorRichTextBox
 {
    public class RichTextBoxColorConstructor
    {
+      private readonly TextBox _textBox;
+      private readonly ListBox _listBox;
+      private readonly RichTextBox _richTextBox;
 
+      // Конструктор, принимающий ссылки на элементы управления
+      public PassLinkControlConstructor(TextBox textBox, ListBox listBox, RichTextBox richTextBox)
+      {
+         _textBox = textBox;
+         _listBox = listBox;
+         _richTextBox = richTextBox;
+      }
+
+      // Метод, добавляет текст в TextBox
+      public void AddTextBoxText(string text)
+      {
+         if (_textBox != null)
+         {
+            // Добавляет строку оставляя предыдущие
+            _textBox.AppendText(text);
+            _textBox.AppendText(Environment.NewLine);
+            // Прокрутка TextBox вниз
+            _textBox.ScrollToCaret();
+         }
+      }
+
+      // Метод, добавляет элемент в ListBox
+      public void AddToListBox(string item)
+      {
+         if (_listBox != null)
+         {
+            _listBox.Items.Add(item);
+            // Прокрутка ListBox вниз
+            _listBox.TopIndex = _listBox.Items.Count - 1;
+         }
+      }
+
+      // Метод, добавляет текст в RichTextBox
+      public void AddToRichTextBox(string text)
+      {
+         if (_richTextBox != null)
+         {
+            _richTextBox.AppendText(text);
+            _richTextBox.AppendText(Environment.NewLine);
+            // Прокрутка RichTextBox вниз
+            _richTextBox.ScrollToCaret();
+         }
+      }
    }
 }
