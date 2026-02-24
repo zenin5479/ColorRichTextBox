3a85bcdf00a8be49e26b6e2283f79a34ca82bc6c
zenin5479 <zenin5479@gmail.com>
2026-02-24 16:19:06 +0300

    Update 13.4 Обновлен метод

--- FormOne.cs
+++ FormOne.cs
@@ -64,7 +64,7 @@ namespace ColorRichTextBox
          // Создаём экземпляр класса и передаём ссылку на элемент управления: RichTextBox
          // Локальная переменная создаётся здесь и живёт только в этом методе
          RichTextBoxColorConstructor constructor = new RichTextBoxColorConstructor(RichTextBoxOne,
-            "Вывод текста через метод TextColorTwo класса RichTextBoxColor\n", Color.Fuchsia);
+            "Вывод текста через метод TextColorConstructorTwo класса RichTextBoxColor\n", Color.Fuchsia);
          constructor.TextColorConstructorTwo();
       }
 
